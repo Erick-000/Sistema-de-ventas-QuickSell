8514864d7cc389f418dc3a42f764df7aa500fd50
Erick-000 <erickmanuel238@gmail.com>
2024-11-09 15:15:43 -0500

    Se agrega el index y el login

---
+++ app/config.php
@@ -0,0 +1,16 @@
+<?php
+
+define('SERVIDOR','localhost');
+define('USUARIO','root');
+define('PASSWORD','');
+define('BD','sistemadeventas');
+
+$servidor = "mysql:dbname=".BD.";host=".SERVIDOR;
+
+try{
+    $pdo = new PDO($servidor, USUARIO, PASSWORD, array(PDO::MYSQL_ATTR_INIT_COMMAND=>"SET NAMES utf8"));
+}catch(PDOException $e){
+    echo "Error al conectarse a la base de datos";
+}
+
+$URL = "http://localhost/www.quicksell.com";
---
+++ app/controllers/login/ingreso.php
@@ -0,0 +1,30 @@
+<?php
+
+include('../../config.php');
+
+$email = $_POST['email'];
+$password_user = $_POST['password_user'];
+
+$contador = 0;
+$sql = "SELECT * FROM tb_usuarios WHERE email = '$email' AND password_user = '$password_user'";
+
+$query = $pdo->prepare($sql);
+$query->execute();
+
+$usuarios = $query->fetchAll(PDO::FETCH_ASSOC);
+foreach($usuarios as $usuario){
+    $contador = $contador + 1;
+    $email_tabla = $usuario['email'];
+    $nombres_tabla = $usuario['nombres'];
+}
+
+if($contador == 0){
+    echo "Datos de inicio de sesión incorrectos";
+}else{
+    echo "Datos de incio de sesión correctos";
+
+    //Creación de la sesión
+    session_start();
+    $_SESSION['sesion_email'] = $email;
+    header('Location: '.$URL.'/index.php');
+}
